b6877b4e859e1f9e0e15f57eb133624387e9734a
Rémi Prévost <remi@exomel.com>
2013-07-30 10:15:06 -0400

    Refactor all output calls

--- lib/hanzo.rb
+++ lib/hanzo.rb
@@ -6,3 +6,27 @@ require 'hanzo/base'
 require 'hanzo/cli'
 require 'hanzo/heroku'
 require 'hanzo/version'
+
+module Hanzo
+  def self.run(command)
+    print(command, :green)
+    `#{command}`
+  end
+
+  def self.print(text, *colors)
+    colors = colors.map { |c| HighLine.const_get(c.to_s.upcase) }
+    HighLine.say HighLine.color("       #{text}", *colors)
+  end
+
+  def self.title(text)
+    HighLine.say HighLine.color("-----> #{text}", :blue)
+  end
+
+  def self.agree(question)
+    HighLine.agree "       #{question} "
+  end
+
+  def self.ask(question, &blk)
+    HighLine.ask "-----> #{question} ", &blk
+  end
+end
--- lib/hanzo/modules/installers/labs.rb
+++ lib/hanzo/modules/installers/labs.rb
@@ -2,10 +2,10 @@ module Hanzo
   module Installers
     module Labs
       def install_labs
-        puts '-----> Activating Heroku Labs'
+        Hanzo.title 'Activating Heroku Labs'
 
         Hanzo::Heroku.available_labs.each do |name, description|
-          if agree("       Add #{name}? ")
+          if Hanzo.agree("Add #{name}?")
             Hanzo::Installers::Remotes.environments.each_pair do |env, app|
               Hanzo::Installers::Labs.enable(env, lab)
             end
@@ -14,8 +14,8 @@ module Hanzo
       end
 
       def self.enable(env, lab)
-        `heroku labs:enable #{lab} --remote #{env}`
-        puts "       - Enabled for #{env}"
+        Hanzo.run "heroku labs:enable #{lab} --remote #{env}"
+        Hanzo.print "- Enabled for #{env}"
       end
     end
   end
