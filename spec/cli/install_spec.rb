7fa3f8f18c4f4f8673657d21a71e5f5e322141bf
Samuel Garneau <sgarneau@mirego.com>
2013-12-16 10:45:33 -0500

    Add tests for Hanzo::Installers::Remotes

--- spec/cli/install_spec.rb
+++ spec/cli/install_spec.rb
@@ -2,21 +2,19 @@ require 'spec_helper'
 
 describe Hanzo::CLI do
   describe :install do
-    let(:install!) { Hanzo::CLI.new(['install', 'labs']) }
+    let(:install!) { Hanzo::CLI.new(['install', type]) }
     let(:heroku_remotes) { { 'production' => 'heroku-app-production', 'qa' => 'heroku-app-qa' } }
-    let(:labs_title) { 'Activating Heroku Labs' }
-
-    before do
-      Hanzo::Installers::Remotes.stub(:environments).and_return(heroku_remotes)
-    end
 
     describe :labs do
+      let(:type) { 'labs' }
+      let(:labs_title) { 'Activating Heroku Labs' }
       let(:available_labs) { { 'user-env-compile' => 'Description' } }
 
       let(:enable_labs_cmd) { "heroku labs:enable" }
       let(:enable_labs_info) { '- Enabled for' }
 
       before do
+        Hanzo::Installers::Remotes.stub(:environments).and_return(heroku_remotes)
         Hanzo::Heroku.stub(:available_labs).and_return(available_labs)
         Hanzo.should_receive(:title).with(labs_title)
 
@@ -32,6 +30,36 @@ describe Hanzo::CLI do
 
       it 'should install specified labs for each environment' do
         install!
+      end
+    end
+
+    describe :remotes do
+      let(:type) { 'remotes' }
+      let(:create_remotes_title) { 'Creating git remotes' }
+
+      before { Hanzo.should_receive(:title).with(create_remotes_title) }
+
+      context '.heroku-remotes exists' do
+        before do
+          Hanzo::Installers::Remotes.stub(:environments).and_return(heroku_remotes)
+          heroku_remotes.each do |env, app|
+            Hanzo.should_receive(:print).with("Adding #{env}")
+            Hanzo.should_receive(:run).with("git remote rm #{env} 2>&1 > /dev/null")
+            Hanzo.should_receive(:run).with("git remote add #{env} git@heroku.com:#{app}.git")
+          end
+        end
+
+        it 'should install git remotes' do
+          install!
+        end
+      end
+
+      context '.heroku-remotes file is missing' do
+        before { Hanzo.should_receive(:print).twice }
+
+        it 'should display error message' do
+          lambda { install! }.should raise_error SystemExit
+        end
       end
     end
   end
